c202b06785b1f971ad05c5915c35cbee181b167e
Aman Sharma <amansharma.pcmip@outlook.com>
2019-12-26 16:00:54 +0530

    Intro to basic numpy

---
+++ numpy_intro.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+array2d=np.array([[4,4,4,4,4],[5,5,5,5,5]])
+print(array2d)
+
+print(array2d.ndim) #dimensions
+print(array2d.shape) #shape
+
+print(len(array2d)) #number of elements
+print(array2d.size) #number of nested elements
+
+print(type(array2d))
+print(array2d.dtype) #internal data type
